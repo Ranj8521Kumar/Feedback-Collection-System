ba8ceabb1e589f361c2323690059630cea3129c2
Ranj8521Kumar <22it3037@rgipt.ac.in>
2024-10-10 20:42:43 +0530

    frontend setup/code complete

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FeedbackForm from './components/feedbackForm.js';
+
+function App(){
+  return (
+      <div className='App'>
+        <FeedbackForm/>
+      </div>
+  );
+}
+
+export default App;
